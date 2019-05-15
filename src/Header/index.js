327022115fba981f3507f41b172e2ddbced0f0ba
Matthew Gonczar <mattisagumball@gmail.com>
2019-05-15 11:53:35 -0500

    bug squashed

--- src/Header/index.js
+++ src/Header/index.js
@@ -7,7 +7,7 @@ class Header extends React.Component {
 		this.handleLoginClick = this.handleLoginClick.bind(this);
     	this.handleLogoutClick = this.handleLogoutClick.bind(this);
     	this.state = {
-    		isLoggedIn: false
+    		isLoggedIn: false,
     		user: ''
     	};
 	}
