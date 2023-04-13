92e6cd1f7bfda99ac5555caa4e1545a87108f04d
GonczarM <matthew.gonczar@gmail.com>
2023-04-13 09:30:50 -0600

    removed vite css

--- src/utils/users-service.js
+++ src/utils/users-service.js
@@ -7,14 +7,10 @@ export async function login(userData) {
 }
 
 export function getToken() {
-  // getItem returns null if there's no string
   const token = localStorage.getItem('token');
   if (!token) return null;
-  // Obtain the payload of the token
-  const payload = JSON.parse(atob(token.split('.')[1]));
-  // A JWT's exp is expressed in seconds, not milliseconds, so convert
+  const payload = JSON.parse(window.atob(token.split('.')[1]));
   if (payload.exp < Date.now() / 1000) {
-    // Token has expired - remove it from localStorage
     localStorage.removeItem('token');
     return null;
   }
@@ -23,8 +19,7 @@ export function getToken() {
 
 export function getUser() {
   const token = getToken();
-  // If there's a token, return the user in the payload, otherwise return null
-  return token ? JSON.parse(atob(token.split('.')[1])).user : null;
+  return token ? JSON.parse(window.atob(token.split('.')[1])).user : null;
 }
 
 export function logOut() {
